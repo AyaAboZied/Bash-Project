ae37fd37479b6ebec240c521a1905be930437aac
AyaAboZied <68289149+AyaAboZied@users.noreply.github.com>
2022-12-31 17:48:30 +0200

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,46 +1,44 @@
-
 echo "Please,Enter DB folder name "
 read name
 function checkName {
+if [[ !($name =~ ^[A-Za-z]*$) ]];then
+echo "invaild name"
+return
+else
 for i in *
 do
-if [ $i != $name ]; then
+if [[ $i != $name ]]; then
  mkdir $name
- cd ./$name
  break
-<<c
-elif [ -d "$i" ]&&[  $i = $name ];then
-   echo "already exist as Directory"
-   cd ./$name
 else
-echo "already exist as file ,enter name again" 
- break
-c
-else
- cd ./$name
 echo "It's already Exit"
 break
 fi
 done
+fi
 }
 
 function Drop {
 echo "what is the DB you want to remove "
 read name
+rr=0
 for i in *
 do
-if [ $i = $name ]; then
+if [[ $i = $name ]]; then
  rm -r $name
 echo "Done removing"
-else
- echo "Enter correct name"
+let rr=$rr+2
 break
 fi
 done
+if [[ $rr<1 ]];then
+echo "invalid"
+fi
 }
 checkName 
 
-
+while true
+do
 select choice in Create list Drop connect
 do
 case $choice in
@@ -50,17 +48,17 @@ read name
 checkName
 break;;
 list)
-echo "this the list of DB"
+ cd $name
+echo "this the list of ${name} DB "
 ls -F | grep /
 break;;
 Drop)
 Drop
 break;;
 connect)
-cd ..
-sh ./r.sh
+ cd $name
+source ~/Desktop/r.sh
 break;;
 esac
 done
-
-
+done
